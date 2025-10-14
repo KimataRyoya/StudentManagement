cc8708aaacd4a8e9897da44d04111606849ba638
Kimata <kimataroudayoo@yahoo.co.jp>
2025-10-14 20:21:20 +0900

    MapのPOSTとGETを実装

--- src/main/java/raisetech/studentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/studentManagement/StudentManagementApplication.java
@@ -1,11 +1,12 @@
 package raisetech.studentManagement;
 
+import java.util.HashMap;
 import java.util.Map;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -15,10 +16,17 @@ public class StudentManagementApplication {
 	private String name = "Kubo Taito";
 	private int age = 50;
 
+	private Map<String,Integer> studentMap = new HashMap<>();
+
+	public void studentMapPut(){
+		studentMap.put("KIMATA",29);
+		studentMap.put("HINA",22);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
-		
+
+
 	}
 	@GetMapping("/studentInfo")
 	public String getStudentInfo() {
@@ -32,5 +40,15 @@ public class StudentManagementApplication {
 	@PostMapping("/studentName")
 	public void setStudentName(String name){
 		this.name = name;
+	}
+	@GetMapping("/studentMap")
+	public Map<String, Integer> getStudentMap(){
+		studentMapPut();
+		return studentMap;
+	}
+	@PostMapping("/studentMap")
+	public void setStudentMap(@RequestParam String name,@RequestParam Integer age){
+		studentMap.put(name, age);
+		
 	}
 }
