72d33e10fd7c6858e4c534726afd6bfd1e28bbf3
Kimata <kimataroudayoo@yahoo.co.jp>
2025-11-17 16:30:39 +0900

    お試しでセレクトボックスを実装

--- src/main/java/raisetech/studentManagement/data/Student.java
+++ src/main/java/raisetech/studentManagement/data/Student.java
@@ -18,4 +18,5 @@ public class Student {
   private String remark;
   private boolean isDeleted;
 
+
 }
--- src/main/java/raisetech/studentManagement/service/StudentService.java
+++ src/main/java/raisetech/studentManagement/service/StudentService.java
@@ -1,10 +1,13 @@
 package raisetech.studentManagement.service;
 
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import raisetech.studentManagement.data.Student;
 import raisetech.studentManagement.data.StudentCourse;
+import raisetech.studentManagement.domain.StudentDetail;
 import raisetech.studentManagement.repository.StudentRepository;
 
 @Service
@@ -23,5 +26,17 @@ public class StudentService {
 
   public List<StudentCourse> searchStudentCourseList() {
     return repository.searchStudentCourseList();
+  }
+
+  @Transactional
+  public void registerStudent(StudentDetail studentDetail) {
+    repository.registerStudent(studentDetail.getStudent());
+    // TODO:コース情報登録も行う。
+    for (StudentCourse studentCourse : studentDetail.getStudentCourses()) {
+      studentCourse.setStudentsId(studentDetail.getStudent().getId());
+      studentCourse.setStart(LocalDate.now());
+      studentCourse.setEnd(LocalDate.now().plusYears(1));
+      repository.registerStudentCourses(studentCourse);
+    }
   }
 }
